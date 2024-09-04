294213f8995be96935f78150cf7c563e6204f142
marzzd <162103888+marzzd@users.noreply.github.com>
2024-09-04 14:18:00 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,9 +16,9 @@ add_crontab_task() {
   if ! grep -q "$txt" "$file"; then
     echo "*/12 * * * * if ! pgrep -x web; then nohup $txt >/dev/null 2>&1 & fi" >> $file
     crontab $file
-    echo -e "\e[1;32mCrontab 任务添加完成\e[0m"
+    echo "任务添加完成"
   else
-      echo -e "\e[1;32mCrontab 任务已存在，无需添加\e[0m"
+      echo "任务已存在，无需添加"
   fi
   rm $file
 }
